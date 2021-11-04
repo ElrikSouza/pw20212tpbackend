42b21c29c69498e0b875af855aa3df113876b44c
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-04 13:53:53 -0400

    created a migration for the order table

---
+++ src/seq/database/migrations/3-compra.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Compra", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      data: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, _) => {
+    await queryInterface.dropTable("Compra");
+  },
+};
