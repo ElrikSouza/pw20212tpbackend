395086e09e03775113a4fa8f5858ab521b8bec3a
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-07 18:51:22 -0400

    TipoUsuario Seeder: create the first collaborator account based on the variables in process.env

--- src/seq/database/seeders/tipousuario.cjs
+++ src/seq/database/seeders/tipousuario.cjs
@@ -1,5 +1,32 @@
+const getFromEnvOrFail = (key) => {
+  if (!process.env[key]) {
+    throw new Error(`Key ${key} does not exist in process.env`);
+  }
+
+  return process.env[key];
+};
+
+const getAdmAccountFromEnv = () => {
+  if (process.env.NODE_ENV == "local") {
+    const { config } = require("dotenv");
+    config({ path: ".local.env" });
+  }
+
+  const admAccount = {
+    id: 1,
+    tipoUsuarioId: 2,
+    nome: "adm",
+    email: getFromEnvOrFail("ADM_EMAIL"),
+    senha: getFromEnvOrFail("ADM_BCRYPT_PASS"),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  return admAccount;
+};
+
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface, _) => {
     await queryInterface.bulkInsert("TipoUsuario", [
       {
         id: 1,
@@ -14,11 +41,15 @@ module.exports = {
         updatedAt: new Date(),
       },
     ]);
+
+    await queryInterface.bulkInsert("Usuario", [getAdmAccountFromEnv()]);
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface, _) => {
     await queryInterface.bulkDelete("TipoUsuario", {
       rotulo: ["USER", "ADM"],
     });
+
+    await queryInterface.bulkDelete("Usuario", { where: { id: 1 } });
   },
 };
