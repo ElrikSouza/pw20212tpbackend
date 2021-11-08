4c1e4c8c2aebe5a5de9dd5491b31a9f62502af2a
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-08 15:43:30 -0400

    added cors package

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Express, { json } from "express";
 import { CollaboratorsModule } from "./collaborators/collaborators-routes.js";
 import { errorHandlingMiddleware } from "./errors/error-handler.middleware.js";
 import { UsersModule } from "./users/users-routes.js";
+import cors from "cors";
 
 export const app = Express();
 
 app.use(json());
+app.use(cors({ origin: "*" }));
 app.use(UsersModule);
 app.use(CollaboratorsModule);
 app.use(errorHandlingMiddleware);
