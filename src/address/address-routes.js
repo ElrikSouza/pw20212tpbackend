4d02e9bd2153102de271fa8b424474aed3772508
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-15 17:04:05 -0400

    added missing jwt middleware to the create address endpoint

--- src/address/address-routes.js
+++ src/address/address-routes.js
@@ -4,5 +4,10 @@ import { AddressController } from "./address-controller.js";
 
 export const AddressModule = Router();
 
-AddressModule.post("/addresses", AddressController.createAddress);
+AddressModule.post(
+  "/addresses",
+  jwtMiddleware,
+  AddressController.createAddress
+);
+
 AddressModule.get("/addresses", jwtMiddleware, AddressController.getAddresses);
