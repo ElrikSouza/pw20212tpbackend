b9391fdaf90f79048567d9089d93b3c7f583ef26
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-16 19:37:24 -0400

    bug fixing

--- src/address/address-repo.js
+++ src/address/address-repo.js
@@ -1,13 +1,13 @@
 import { Address } from "./address-model.js";
 
 const getAddresses = async (userId) => {
-  const address = await Address.findAll({ where: { usuarioId: userId } });
+  const addresses = await Address.findAll({ where: { usuarioId: userId } });
 
-  if (address == null) {
-    return { address: [] };
+  if (addresses == null) {
+    return { addresses: [] };
   }
 
-  return { address };
+  return { addresses };
 };
 
 const createAddresss = async (address, userId) => {
--- src/orders/orders-routes.js
+++ src/orders/orders-routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
+import { jwtMiddleware } from "../jwt/jwt-middleware.js";
 import { OrdersController } from "./order-controller.js";
 
 export const OrdersModule = Router();
 
-OrdersModule.post("/orders", OrdersController.createOrder);
+OrdersModule.post("/orders", jwtMiddleware, OrdersController.createOrder);
