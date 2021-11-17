7f197c3c67ed360e0581b6799541d3278b37a9c3
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-16 20:49:08 -0400

    refactored buildSchemaValidator function

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 import { ValidationError } from "../errors/errors.js";
 
 export const buildSchemaValidator =
-  (schema, options = {}) =>
+  (schema, options = { allowUnknown: true, stripUnknown: true }) =>
   async (value) => {
     try {
       const result = await Joi.attempt(value, schema, options);
