f47cdc150d297d0519860324dd0b25db332a3dd4
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-17 15:35:49 -0400

    fixed the id validator

--- src/helpers/id-validator.js
+++ src/helpers/id-validator.js
@@ -2,5 +2,5 @@ import Joi from "joi";
 import { buildSchemaValidator } from "./validator.js";
 
 export const validateIntId = buildSchemaValidator(
-  Joi.number().integer().min(1)
+  Joi.number().integer().min(1).required()
 );
